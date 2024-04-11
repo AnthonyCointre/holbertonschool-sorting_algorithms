a1596797aed777afcf019b71b4e9b1d121283d0a
AnthonyCointre <8732@holbertonstudents.com>
2024-04-11 15:38:29 +0200

    creation 1-insertion_sort_list.c, 1-O

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
-* insertion_sort_list - Sorts a doubly linked list of integers in
-*                       ascending order using the Insertion sort algorithm.
-* @list: A pointer to the head of the list
+* insertion_sort_list - Sorts a doubly linked list of integers in ascending
+*                       order using the Insertion sort algorithm.
+* @list: A pointer to the first element of the list.
 */
 void insertion_sort_list(listint_t **list)
 {
